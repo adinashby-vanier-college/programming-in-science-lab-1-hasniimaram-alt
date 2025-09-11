cba5741dd0615b273dbe586623869591cd842f17
hasniimaram-alt <hasniimaram@gmail.com>
2025-09-11 18:15:50 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -3,7 +3,7 @@ def hello_world():
     print ("Hello, World!")
   
     
-hello_world()
+
 def input_output():
     name = input ("Enter your name")
     age = int(input ("Enter your age: "))
@@ -13,5 +13,5 @@ def input_output():
     print(f"You are {age} years old.")
     print(f"Your height is {height} meters.")
     
-input_output()
+
 
